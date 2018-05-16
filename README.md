b0ab4b79d839e266caad6e801c4e49fbd120d4cc
张明凯 <zhangmingkai@cnnic.cn>
2018-05-16 16:09:55 +0800

    add scripts

--- README.md
+++ README.md
@@ -7,6 +7,9 @@ Using Free GeoIP databases, transfer to redis database for api query
 
 GeoLite2 databases are free IP geolocation databases comparable to, but less accurate than, MaxMind’s GeoIP2 databases. The GeoLite2 Country and City databases are updated on the first Tuesday of each month. The GeoLite2 ASN database is updated every Tuesday.
 
+[download](https://dev.maxmind.com/geoip/geoip2/geolite2/)
+
 #### License
 This product includes GeoLite2 data created by MaxMind, available from [http://www.maxmind.com](http://www.maxmind.com)
+
 
--- scripts/download_geoip2_file.sh
+++ scripts/download_geoip2_file.sh
@@ -2,20 +2,66 @@
 
 BASEDIR=$(dirname "$0")
 DATA_FOLDER=$(dirname "$0")/../data/
-GEOIP2_DOWNLOAD_URL=http://geolite.maxmind.com/download/geoip/database/GeoLiteCity_CSV/GeoLiteCity-latest.zip
+BACKUP_FOLDER=$(dirname "$0")/../data/backup
+TEMP_FOLDER=$(dirname "$0")/../data/temp
 
-# check remote file etag or last modify if the same then quit download process
+	
+[ ! -d $DATA_FOLDER ] && mkdir -p $DATA_FOLDER
+[ ! -d $BACKUP_FOLDER ] && mkdir -p $BACKUP_FOLDER
+[ ! -d $TEMP_FOLDER ] && mkdir -p $TEMP_FOLDER
 
-# end
 
-cd $DATA_FOLDER
+GEOIP2_DOWNLOAD_URL=http://geolite.maxmind.com/download/geoip/database/GeoLite2-City-CSV.zip
+GEOIP2_DOWNLOAD_MD5_URL=http://geolite.maxmind.com/download/geoip/database/GeoLite2-City-CSV.zip.md5
+GEOIP2_DATA_FOLDER=GeoLite2-City-CSV
 
-wget $GEOIP2_DOWNLOAD_URL 
-failed=`echo $?`
-if [ $failed == "0" ]
-then 
-  rm -rf GeoLiteCity_*
-  unzip GeoLiteCity-latest.zip 
-else
-  echo "ERROR: Can't get http://geolite.maxmind.com/download/geoip/database/GeoLiteCity_CSV/GeoLiteCity-latest.zip"
-fi
+
+GEOIP2_ASN_DOWNLOAD_URL=http://geolite.maxmind.com/download/geoip/database/GeoLite2-ASN-CSV.zip
+GEOIP2_ASN_DOWNLOAD_MD5_URL=http://geolite.maxmind.com/download/geoip/database/GeoLite2-ASN-CSV.zip.md5
+GEOIP2_ASN_DATA_FOLDER=GeoLite2-ASN-CSV
+
+# check remote file md5 if the same then return 0 
+check_md5()
+{
+  newRemoteMD5=$(wget $1 -q -O -)
+  if [ "$2" = "$newRemoteMD5" ];then
+    return 0
+  else
+    return 1
+  fi
+}
+
+# Download file from url
+download_file()
+{
+  FILE_URL=$1
+  filename=`basename $FILE_URL`
+  md5filePath=$BACKUP_FOLDER/$filename.md5
+  if [ -f $md5filePath ];then
+    oldMD5Value=`cat $md5filePath`
+    check_md5 $2 $oldMD5Value
+    if [ $? -eq 0 ];then 
+      return 0
+    fi
+   
+  fi
+  # file has changed md5 file value not equal 
+  result=$(wget $1 -O $TEMP_FOLDER/$filename)
+  if [ $? -eq 0 ];then 
+    echo "download sucess"
+    rm -rf $DATA_FOLDER/$3
+    unzip -o $TEMP_FOLDER/$filename -d $DATA_FOLDER
+    # mv $3* $3
+    find ./data -type d -name "$3_*" -exec mv {} $DATA_FOLDER/$3 \;
+    mv -f $TEMP_FOLDER/$filename $BACKUP_FOLDER/$filename
+    newMD5Value=`md5sum $BACKUP_FOLDER/$filename | awk '{ print $1 }'`
+    echo $newMD5Value > $md5filePath
+  else
+    echo "ERROR: Can't download $filename"
+  fi
+
+}
+
+download_file $GEOIP2_ASN_DOWNLOAD_URL $GEOIP2_ASN_DOWNLOAD_MD5_URL $GEOIP2_ASN_DATA_FOLDER
+download_file $GEOIP2_DOWNLOAD_URL $GEOIP2_DOWNLOAD_MD5_URL $GEOIP2_DATA_FOLDER
+
