de2411d3ebbf47ecce199f502567fea689ec7380
zhangmingkai4315 <zhangmingkai19892013@gmail.com>
2018-05-16 20:18:36 +0800

    add macosx download support

--- scripts/updatefile.sh
+++ scripts/updatefile.sh
@@ -5,6 +5,13 @@ DATA_FOLDER=$(dirname "$0")/../data/
 BACKUP_FOLDER=$(dirname "$0")/../data/backup
 TEMP_FOLDER=$(dirname "$0")/../data/temp
 
+platform='unknown'
+unameStr=`uname`
+if [[ "$unameStr" == "Linux" ]];then
+  platform="linux"
+elif [[ "$unameStr" == "Darwin" ]];then
+  platform="macosx"
+fi
 	
 [ ! -d $DATA_FOLDER ] && mkdir -p $DATA_FOLDER
 [ ! -d $BACKUP_FOLDER ] && mkdir -p $BACKUP_FOLDER
@@ -54,7 +61,11 @@ download_file()
     # mv $3* $3
     find ./data -type d -name "$3_*" -exec mv {} $DATA_FOLDER/$3 \;
     mv -f $TEMP_FOLDER/$filename $BACKUP_FOLDER/$filename
-    newMD5Value=`md5sum $BACKUP_FOLDER/$filename | awk '{ print $1 }'`
+    if [[ "$platform" == "macosx" ]];then
+      newMD5Value=`md5 $BACKUP_FOLDER/$filename | awk -F " = " '{print $2}'`
+    else
+      newMD5Value=`md5sum $BACKUP_FOLDER/$filename | awk '{ print $1 }'`
+    fi
     echo $newMD5Value > $md5filePath
   else
     echo "ERROR: Can't download $filename"
